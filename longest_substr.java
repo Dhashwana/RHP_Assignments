9c8b3651f531ff8156a346304b07fbb0c3ab7bba
Dhashwana <25cs048@rmkcet.ac.in>
2026-06-12 12:52:23 +0530

    10-06-2026 Programs

--- longest_substr.java
+++ longest_substr.java
@@ -8,7 +8,7 @@ public class Main{
         int a=s1.length(), b=s2.length(), max=0;
         int[][] dp=new int[a+1][b+1];
         for(int i=1; i<=a; i++){
-            for(int j=1; j<=b; j++){
+             for(int j=1; j<=b; j++){
                 if(s1.charAt(i-1)==s2.charAt(j-1)){
                     dp[i][j]=1+dp[i-1][j-1];
                     max=Math.max(max, dp[i][j]);
